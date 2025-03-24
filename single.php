78942dfbb7285f9a773156c37178bf665c05f115
quanghacker97 <quanghacker97@gmail.com>
2025-03-24 14:13:22 +0700

    chỉnh lại breadcumb

---
+++ single.php
@@ -0,0 +1,35 @@
+<?php include "./header.php" ?>
+<style>
+    .pageinside h1 {
+        font-size: 1.75em;
+        color: #000;
+        font-weight: 700;
+        margin: .25em 0;
+    }
+
+    .productnews .h2 {
+        font-size: 1em;
+        font-weight: 700;
+        color: #016e3f;
+        padding: .5em 0;
+        text-align: center;
+        margin-bottom: .5em;
+    }
+</style>
+<main>
+    <div class="container pageinside  ml-auto mr-auto mt-3 mb-3 pt-3 pb-3 d-flex flex-wrap clearfix">
+        <article class="boxcontents col-12 col-md-9">
+            <h1 class="color font-weight-bold" data-pw="">Top 3 Loại Hộp Carton phổ biến nhất hiện nay</h1>
+            <div class="breadcrumb">
+                <ul>
+                    <li><a href="#"><img src="images/house.png" width="20px" height="auto" alt="">Trang chủ</a></li>
+                    <li>Top 3 Loại Hộp Carton phổ biến nhất hiện nay</li>
+                </ul>
+            </div>
+        </article>
+        <div class="productnews p-0 ps-3 col-12 col-md-3 clearfix">
+            <div class="h2">SẢN PHẨM DÀNH RIÊNG CHO BẠN</div>
+        </div>
+    </div>
+</main>
+<?php include "./footer.php" ?>
