8bccc536b89daa872dcb922a0bc9f0fcc4084f63
quanghacker97 <quanghacker97@gmail.com>
2025-03-24 11:36:49 +0700

    màn archive - danh mục bài viết

---
+++ archive.php
@@ -0,0 +1,114 @@
+<?php include "./header.php" ?>
+
+<style>
+    .boxnews .items {
+        transition: all 0.3s;
+    }
+
+    .boxnews .items:hover {
+        transform: translateY(-5px);
+    }
+
+    .boxnews .imgset img {
+        width: 100%;
+    }
+
+    .boxnews .items .nameit h3 {
+        height: 3em;
+    }
+
+    .boxnews .items .nameit h3 a {
+        color: #016e3f;
+    }
+
+    .boxnews .desnews {
+        height: 4.5em;
+    }
+
+    .boxnews .dateviews {
+        color: #666;
+    }
+</style>
+<main>
+    <div class="container">
+        <div class="boxnews row mt-3 px-2">
+            <div class="items mb-3 col-6 col-md-3 col-sm-4">
+                <a class="imgset" href="#">
+                    <img width="350" height="275" src="images/bg-feadback.jpg" alt="Top 3 Loại Hộp Carton phổ biến nhất hiện nay" class="loaded">
+                </a>
+                <div class="nameit py-2">
+                    <h3 class="font-weight-bold mb-2 fs-15 text-line-clamp-2 line-height-24"><a href="#">Top 3 Loại Hộp Carton phổ biến nhất hiện nay</a></h3>
+                    <p class="desnews my-2 fs-15 text-justify text-line-clamp-3 line-height-24">Hộp carton là một trong những loại bao bì đóng gói được sử dụng phổ biến nhất hiện nay. Nhờ tính đa dụng, bền nhẹ và thân thiện với môi trường, hộp carton được ứng dụng rộng rãi trong nhiều ngành hàng từ thương mại điện tử, quà tặng, thực phẩm đến đóng gói sản phẩm cao cấp.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        <a class="views" href="#">[Chi tiết]</a>
+                    </p>
+                    <p class="dateviews m-0 fs-13 fst-italic">Date: 12/03/2025 &nbsp;&nbsp; Views: 85</p>
+                </div>
+            </div>
+            <div class="items mb-3 col-6 col-md-3 col-sm-4">
+                <a class="imgset" href="#">
+                    <img width="350" height="275" src="images/bg-feadback.jpg" alt="Top 3 Loại Hộp Carton phổ biến nhất hiện nay" class="loaded">
+                </a>
+                <div class="nameit py-2">
+                    <h3 class="font-weight-bold mb-2 fs-15 text-line-clamp-2 line-height-24"><a href="#">Top 3 Loại Hộp Carton phổ biến nhất hiện nay</a></h3>
+                    <p class="desnews my-2 fs-15 text-justify text-line-clamp-3 line-height-24">Hộp carton là một trong những loại bao bì đóng gói được sử dụng phổ biến nhất hiện nay. Nhờ tính đa dụng, bền nhẹ và thân thiện với môi trường, hộp carton được ứng dụng rộng rãi trong nhiều ngành hàng từ thương mại điện tử, quà tặng, thực phẩm đến đóng gói sản phẩm cao cấp.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        <a class="views" href="#">[Chi tiết]</a>
+                    </p>
+                    <p class="dateviews m-0 fs-13 fst-italic">Date: 12/03/2025 &nbsp;&nbsp; Views: 85</p>
+                </div>
+            </div>
+            <div class="items mb-3 col-6 col-md-3 col-sm-4">
+                <a class="imgset" href="#">
+                    <img width="350" height="275" src="images/bg-feadback.jpg" alt="Top 3 Loại Hộp Carton phổ biến nhất hiện nay" class="loaded">
+                </a>
+                <div class="nameit py-2">
+                    <h3 class="font-weight-bold mb-2 fs-15 text-line-clamp-2 line-height-24"><a href="#">Top 3 Loại Hộp Carton phổ biến nhất hiện nay</a></h3>
+                    <p class="desnews my-2 fs-15 text-justify text-line-clamp-3 line-height-24">Hộp carton là một trong những loại bao bì đóng gói được sử dụng phổ biến nhất hiện nay. Nhờ tính đa dụng, bền nhẹ và thân thiện với môi trường, hộp carton được ứng dụng rộng rãi trong nhiều ngành hàng từ thương mại điện tử, quà tặng, thực phẩm đến đóng gói sản phẩm cao cấp.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        <a class="views" href="#">[Chi tiết]</a>
+                    </p>
+                    <p class="dateviews m-0 fs-13 fst-italic">Date: 12/03/2025 &nbsp;&nbsp; Views: 85</p>
+                </div>
+            </div>
+            <div class="items mb-3 col-6 col-md-3 col-sm-4">
+                <a class="imgset" href="#">
+                    <img width="350" height="275" src="images/bg-feadback.jpg" alt="Top 3 Loại Hộp Carton phổ biến nhất hiện nay" class="loaded">
+                </a>
+                <div class="nameit py-2">
+                    <h3 class="font-weight-bold mb-2 fs-15 text-line-clamp-2 line-height-24"><a href="#">Top 3 Loại Hộp Carton phổ biến nhất hiện nay</a></h3>
+                    <p class="desnews my-2 fs-15 text-justify text-line-clamp-3 line-height-24">Hộp carton là một trong những loại bao bì đóng gói được sử dụng phổ biến nhất hiện nay. Nhờ tính đa dụng, bền nhẹ và thân thiện với môi trường, hộp carton được ứng dụng rộng rãi trong nhiều ngành hàng từ thương mại điện tử, quà tặng, thực phẩm đến đóng gói sản phẩm cao cấp.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        <a class="views" href="#">[Chi tiết]</a>
+                    </p>
+                    <p class="dateviews m-0 fs-13 fst-italic">Date: 12/03/2025 &nbsp;&nbsp; Views: 85</p>
+                </div>
+            </div>
+            <div class="items mb-3 col-6 col-md-3 col-sm-4">
+                <a class="imgset" href="#">
+                    <img width="350" height="275" src="images/bg-feadback.jpg" alt="Top 3 Loại Hộp Carton phổ biến nhất hiện nay" class="loaded">
+                </a>
+                <div class="nameit py-2">
+                    <h3 class="font-weight-bold mb-2 fs-15 text-line-clamp-2 line-height-24"><a href="#">Top 3 Loại Hộp Carton phổ biến nhất hiện nay</a></h3>
+                    <p class="desnews my-2 fs-15 text-justify text-line-clamp-3 line-height-24">Hộp carton là một trong những loại bao bì đóng gói được sử dụng phổ biến nhất hiện nay. Nhờ tính đa dụng, bền nhẹ và thân thiện với môi trường, hộp carton được ứng dụng rộng rãi trong nhiều ngành hàng từ thương mại điện tử, quà tặng, thực phẩm đến đóng gói sản phẩm cao cấp.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        Trong bài viết này, Dao Carton sẽ giới thiệu 3 loại hộp carton được sử dụng phổ biến hiện nay giúp bạn dễ dàng lựa chọn loại phù hợp nhất với nhu cầu sử dụng.
+                        <a class="views" href="#">[Chi tiết]</a>
+                    </p>
+                    <p class="dateviews m-0 fs-13 fst-italic">Date: 12/03/2025 &nbsp;&nbsp; Views: 85</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<?php include "./footer.php" ?>
