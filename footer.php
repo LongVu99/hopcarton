bdd3aba493c8c9b4bf029adbaf024532b97db7e0
LongVu99 <dinhlong0762@gmail.com>
2025-03-25 11:58:30 +0700

    update

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@
                 <p class="des-footer text-white">Hộp carton chất lượng cao từ Dao Carton, bền bỉ, bảo vệ sản phẩm.
                     Thiết kế đẹp, in ấn đa dạng, nâng tầm thương hiệu.
                     Sẻ chia thành công cùng hộp carton Dao Carton, chọn sự hoàn hảo.</p>
-                <p  class="text-green py-2">Được đại diện bởi:</p>
+                <p class="text-green py-2">Được đại diện bởi:</p>
                 <strong class="text-white  fs-20">Công Ty TNHH Sản Xuất Thương Mại Dịch Vụ Tổng Hợp Dao CARTON</strong>
                 <p class="text-white py-2">Mã số thuế: <strong>1800 1002</strong></p>
                 <p class="text-white">Số 83 đường B13 - KDC 91B, Phường An Khánh, Quận Ninh Kiều, Thành phố Cần Thơ</p>
@@ -24,7 +24,7 @@
 
             <!-- Cột 2: Thông tin liên hệ -->
             <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-lg-0">
-                <h3 class="text-white" >DANH MỤC SẢN PHẨM</h3>
+                <h3 class="text-white">DANH MỤC SẢN PHẨM</h3>
                 <div class="categori-icon">
                     <ul>
                         <li class="fs-16 text-white"><img src="" alt="">Hộp Carton</li>
@@ -39,7 +39,7 @@
 
             <!-- Cột 3: Google Maps -->
             <div class="col-sm-6 col-md-6 col-lg-3 mb-3 mb-3 mb-lg-0">
-                <h3  class="text-white">FANPAGE</h3>
+                <h3 class="text-white">FANPAGE</h3>
                 <img src="images/fanpage-banggo.vn (1).jpg" alt="">
                 <h3 class="text-white mt-3">BẢN QUYỀN - BẢO MẬT</h3>
                 <div class="d-flex align-items-center">
@@ -48,16 +48,17 @@
                 </div>
             </div>
         </div>
-        <div class="text-center mt-3">
-            <p class="fs-13">© 2025 <b>Hupuna Group</b>. All rights reserved.</p>
-        </div>
+
+    </div>
+    <div class="text-center mt-3 copy-right bg-green">
+        <p class="fs-13 text-white">© 2025 <b>Hupuna Group</b>. All rights reserved.</p>
     </div>
 </footer>
 
 <section class="btn-fixed-wrap position-fixed">
-   
+
     <div class="back-to-top text-center mt-2 visibility-hidden opacity-0">
-        <a href="javascript:;">
+        <a href="#">
             <img src="icons/to-top.svg" alt="">
         </a>
     </div>
