e055feccea48896420398399dc2c5b2584bb18ec
quanghacker97 <quanghacker97@gmail.com>
2025-03-25 15:22:47 +0700

    header mobile

--- header.php
+++ header.php
@@ -7,7 +7,8 @@
     <meta name="robots" content="noindex,nofollow" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap-grid.css">
 
     <!-- slickslider css and slickslider js  -->
@@ -104,7 +105,7 @@
                             </ul>
                         </li>
                         <li><a href="#" class="fw-700 text-gray">BĂNG KEO</a>
-                        <ul>
+                            <ul>
                                 <li><a href="#">Thùng carton lớn</a></li>
                                 <li><a href="#">Thùng carton 7 lớp</a></li>
                                 <li><a href="#">Thùng carton 5 lớp</a></li>
@@ -126,11 +127,13 @@
                     <div class="d-flex gap-5 icon-cart-heart">
                         <li>
                             <img src="images/heart1.png" alt="" height="25" width="25">
-                            <span class="number-heart position-absolute text-white fs-14 border-radius-50 d-inline-flex align-items-center justify-content-center fw-400">0</span>
+                            <span
+                                class="number-heart position-absolute text-white fs-14 border-radius-50 d-inline-flex align-items-center justify-content-center fw-400">0</span>
                         </li>
                         <li>
                             <a href=""><img src="images/shopping-bag.png" alt="" height="25" width="25"></a>
-                            <span class="numer-cart position-absolute text-white fs-14 border-radius-50 d-inline-flex align-items-center justify-content-center fw-400">0</span>
+                            <span
+                                class="numer-cart position-absolute text-white fs-14 border-radius-50 d-inline-flex align-items-center justify-content-center fw-400">0</span>
                         </li>
                     </div>
                 </div>
@@ -140,21 +143,116 @@
             </div>
         </div>
         <!-- Menu mobile -->
-        <div class="menu-mobi bg-green d-block d-lg-none">
+        <div class="menu-mobi bg-green d-block d-lg-none menu p-0 m-0">
             <ul class="d-flex align-items-center justify-content-center">
-                <li class="text-center fs-30 w-25"><a href="#" class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative" onclick="toggleModal(true)">Menu</a></li>
-                <li class="text-center fs-30 w-25"><a href="#" class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative">Icon Lượt thích</a></li>
-                <li class="text-center fs-30 w-25"><a href="#" class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative">Icon giỏ hàng</a></li>
-                <li class="text-center fs-30 w-25"><a href="#" class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative">Icon tìm kiếm</a></li>
+                <li class="text-center fs-30 w-25 p-0 m-0"><a href="#"
+                        class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative"
+                        onclick="toggleModal(true)"><img src="icons/menu-white.svg" alt="icon-menu" width="28"></a></li>
+                <li class="text-center fs-30 w-25 p-0 m-0">
+                    <a href="#"
+                        class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative">
+                        <img src="icons/heart-icon.svg" alt="" height="28" width="28">
+                        <!-- <span
+                            class="number-heart position-absolute text-white fs-14 border-radius-50 d-inline-flex align-items-center justify-content-center fw-400">0</span> -->
+                    </a>
+                </li>
+                <li class="text-center fs-30 w-25 p-0 m-0 ">
+                    <a href="#"
+                        class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative">
+                        <img src="icons/bag-icon.svg" alt="" height="28" width="28">
+                        <!-- <span
+                            class="numer-cart position-absolute text-white fs-14 border-radius-50 d-inline-flex align-items-center justify-content-center fw-400">0</span> -->
+                    </a>
+                </li>
+                <li class="text-center fs-30 w-25 p-0 m-0"><a href="#"
+                        class="d-inline-flex py-3 px-2 w-100 text-white align-items-center justify-content-center position-relative"><img
+                            src="icons/search-icon.svg" alt="icon-menu" width="28"></a>
+                </li>
             </ul>
-         </div>
-    </div>
-    <!-- ===Mobile Menu===== -->
-    <div class="modal" id="modal">
-        <div class="modal-content">
-            <button class="close-btn" onclick="toggleModal(false)">X</button>
-            Nội dung modal lớn
+        </div>
+        <!-- ===Modal Menu===== -->
+        <div class="modal" id="modal">
+            <div class="modal-content">
+                <div class="text-center fw-500 fs-35 text-white p-2 bg-green">MENU</div>
+                <ul class="listmenu">
+                    <li><a href="#">TRANG CHỦ</a></li>
+                    <li><a href="#">GIỚI THIỆU</a></li>
+                    <li><a class="pointer" href="#"> SẢN PHẨM</a>
+                        <ul class="submmenu">
+                            <li><a href="#"> Hộp Carton</a>
+                                <ul>
+                                    <li><a href="#" title="Thiết kế hộp carton">Thiết kế hộp
+                                            carton</a></li>
+                                    <li><a href="#" title="Hộp carton nắp rời">Hộp carton nắp
+                                            rời</a></li>
+                                    <li><a href="#" title="Hộp carton lạnh">Hộp carton lạnh</a></li>
+                                    <li><a href="#" title="Hộp carton trắng">Hộp carton trắng</a>
+                                    </li>
+                                    <li><a href="#" title="Hộp carton nhỏ">Hộp carton nhỏ</a></li>
+                                    <li><a href="#" title="Hộp carton nắp gài">Hộp carton nắp
+                                            gài</a></li>
+                                    <li><a href="#" title="Hộp carton đóng hàng">Hộp carton đóng
+                                            hàng</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Thùng Carton</a>
+                                <ul>
+                                    <li><a href="#" title="Thùng carton lớn">Thùng carton lớn</a>
+                                    </li>
+                                    <li><a href="#" title="Thùng carton 7 lớp">Thùng carton 7
+                                            lớp</a></li>
+                                    <li><a href="#" title="Thùng carton 5 lớp">Thùng carton 5
+                                            lớp</a></li>
+                                    <li><a href="#" title="Thùng carton đóng hàng">Thùng
+                                            carton đóng hàng</a></li>
+                                    <li><a href="#" title="Thùng carton chuyển nhà">Thùng
+                                            carton chuyển nhà</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Xốp chống sốc</a>
+                                <ul>
+                                    <li><a href="#" title="Xốp PE">Xốp PE</a></li>
+                                    <li><a href="#" title="Xốp Foam">Xốp Foam</a></li>
+                                    <li><a href="#" title="Xốp bọc hàng">Xốp bọc hàng</a></li>
+                                    <li><a href="#" title="Xốp hơi">Xốp hơi</a></li>
+                                    <li><a href="#" title="Xốp nổ">Xốp nổ</a></li>
+                                    <li><a href="#" title="Bọc chống sốc">Bọc chống sốc</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Băng keo</a>
+                                <ul>
+                                    <li><a href="#" title="Băng keo hàng dễ vỡ">Băng keo hàng dễ
+                                            vỡ</a></li>
+                                    <li><a href="#" title="Băng keo vàng">Băng keo vàng</a></li>
+                                    <li><a href="#" title="Băng keo trong">Băng keo trong</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Màng PE</a>
+                                <ul>
+                                    <li><a href="#" title="Màng co bọc hàng">Màng co bọc hàng</a>
+                                    </li>
+                                    <li><a href="#" title="Màng PE quấn pallet">Màng PE quấn
+                                            pallet</a></li>
+                                    <li><a href="#" title="Cuộn PE">Cuộn PE</a></li>
+                                    <li><a href="#" title="Màng PE bọc hàng">Màng PE bọc hàng</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a href="#">In ấn theo yêu cầu</a>
+                                <ul>
+                                    <li><a href="#" title="In hộp carton">In hộp carton</a></li>
+                                    <li><a href="#" title="In thùng carton">In thùng carton</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="#">BÁO GIÁ</a></li>
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">LIÊN HỆ</a></li>
+                </ul>
+            </div>
+            <button class="close-btn bg-white position-absolute p-2 border-0 cursor-pointer" onclick="toggleModal(false)">
+                <img src="icons/close-icon.svg" alt="btn close" width="20" height="auto">
+            </button>
         </div>
     </div>
-
-    <div class="overlay-menu-m position-fixed top-0 start-0 end-0 bottom-0 opacity-0 visibility-hidden" onclick="close_menu_mobile()"></div>
